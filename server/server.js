fda482700b16e094b5478ce467635758419cf74e
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-07-31 10:49:23 +0700

    implement cloudfared quick tunnel

--- server/server.js
+++ server/server.js
@@ -59,6 +59,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:5176',
+    /\.trycloudflare\.com$/,
   ].filter(Boolean),
   credentials: true
 }));
